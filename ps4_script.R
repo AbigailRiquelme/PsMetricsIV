45439e26be0db1768701f1af3b4c07fe41f04b41
AbigailRiquelme <anaabigailriquelme@gmail.com>
2022-05-10 14:37:01 -0300

    cambios

---
+++ ps4_script.R
@@ -0,0 +1,21 @@
+
+# ECONOMETRIA AVANZADA
+# Profesor: Walter Sosa Escudero
+# Asistente: Gaston Garcia Zavaleta
+
+# Problem Set 4
+# Integrantes: Capriata, Pacheco y Riquelme
+# Fecha de entrega: 20 de mayo de 2022
+
+
+# Antes de comenzar vamos a definir el directorio en el que guardaremos las bases 
+# que vamos a exportar e importar.
+
+
+dir <- 
+dir <- "/Users/Abi/Documents/GitHub/PsMetricsIV"
+dir <-
+
+
+setwd(dir)
+
